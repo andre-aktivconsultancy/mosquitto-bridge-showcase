dd8f6d1178b40654597b88242f1a20c9ea38c402
André Kwakernaak <akwakernaak@oceaneering.com>
2025-05-23 14:12:03 +0200

    chore: update publish commands

--- create-tmux.sh
+++ create-tmux.sh
@@ -10,6 +10,6 @@ tmux new-session \; \
   select-pane -t 1 \; \
   send-keys 'docker exec -it mosquitto-bridge mosquitto_sub -h localhost -t "#" -t "\$SYS/connect/#" -v' C-m \; \
   select-pane -t 2 \; \
-  send-keys 'docker exec -it mosquitto-remote mosquitto_pub -h localhost -t "test/topic" -m "hello from remote"' \; \
+  send-keys 'docker exec -it mosquitto-remote mosquitto_pub -h localhost -t "test/both" -m "hello from remote"' \; \
   select-pane -t 3 \; \
-  send-keys 'docker exec -it mosquitto-bridge mosquitto_pub -h localhost -t "test/topic" -m "hello from bridge"'
+  send-keys 'docker exec -it mosquitto-bridge mosquitto_pub -h localhost -t "test/both" -m "hello from bridge"'
